648d9171423789a25f13233d5b45d1768b466aff
dangduy <dangduykhmt@gmail.com>
2025-09-26 10:41:48 +0900

    commit master v3

--- index.php
+++ index.php
@@ -47,6 +47,7 @@ if (!defined('CAKE_CORE_INCLUDE_PATH')) {
 	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
 	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
 	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
 }
 
 require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
